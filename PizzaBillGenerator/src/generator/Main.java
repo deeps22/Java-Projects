10036585b941e5398ffd260ed1cd5ca57b0112a4
deeps22 <72496039+deeps22@users.noreply.github.com>
2024-05-10 20:30:28 +0530

    uploaded

---
+++ PizzaBillGenerator/src/generator/Main.java
@@ -0,0 +1,16 @@
+package generator;
+
+public class Main {
+	public static void main(String[] args) {
+//		Pizza basePizza = new Pizza(true);
+//		basePizza.addExtraToppings();
+//		basePizza.addExtraCheese();
+//		basePizza.takeAway();
+//		basePizza.getBill();
+		
+		DeluxPizza dp = new DeluxPizza(true);
+		dp.addExtraCheese();
+		dp.addExtraToppings();
+		dp.getBill();
+	}
+}
